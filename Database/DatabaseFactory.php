2405ce822ae39a0115b11dd5fb51fac3a9cc5fb2
Neandher Carlos <neandher89@gmail.com>
2016-06-10 09:49:56 -0300

    ...

--- Database/DatabaseFactory.php
+++ Database/DatabaseFactory.php
@@ -5,57 +5,58 @@
  *
  * @author Neandher
  */
+class DatabaseFactory
+{
 
-    class DatabaseFactory {
-        
-        public static function factory( $key ) {                                                
-                
-            if( $key == null ){
-            
-                $key = DB_TYPE;                               
-            }
-            
-            switch( $key ) {
+    public static function factory($key)
+    {
 
-                case 'db_mysql': 
+        if ($key == null) {
 
-                  //code
-
-                break;
-
-                case 'db_mysqlPDO': 
-
-                    if( !Registry::getInstance()->isValid($key) )
-                            Registry::getInstance()->set($key, new MySqlPDO());
-
-                    return Registry::getInstance()->get($key);
-
-                break;
-                
-                case 'db_sqlsrvPDO': 
-
-                    if( !Registry::getInstance()->isValid($key) )
-                            Registry::getInstance()->set($key, new SqlSrvPDO());
-
-                    return Registry::getInstance()->get($key);
-
-                break;
-                
-                case 'db_sqlsrvPDO2': 
-
-                    if( !Registry::getInstance()->isValid($key) )
-                            Registry::getInstance()->set($key, new SqlSrvPDO2());
-
-                    return Registry::getInstance()->get($key);
-
-                break;
-                
-                case 'db_mssql': 
-
-                    //code
-
-                break;
-            }             
+            $key = DB_TYPE;
         }
-        
+
+        switch ($key) {
+
+            case 'db_mysql':
+
+                //code
+
+                break;
+
+            case 'db_mysqlPDO':
+
+                if (!Registry::getInstance()->isValid($key))
+                    Registry::getInstance()->set($key, new MySqlPDO());
+
+                return Registry::getInstance()->get($key);
+
+                break;
+
+            case 'db_sqlsrvPDO':
+
+                if (!Registry::getInstance()->isValid($key))
+                    Registry::getInstance()->set($key, new SqlSrvPDO());
+
+                return Registry::getInstance()->get($key);
+
+                break;
+
+            case 'db_sqlsrvPDO2':
+
+                if (!Registry::getInstance()->isValid($key))
+                    Registry::getInstance()->set($key, new SqlSrvPDO2());
+
+                return Registry::getInstance()->get($key);
+
+                break;
+
+            case 'db_mssql':
+
+                //code
+
+                break;
+        }
     }
+
+}
